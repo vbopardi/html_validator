518df089e966839133635ecf5d49e40f19fcc793
vbopardi <vbopard@gmail.com>
2021-02-16 00:27:04 -0800

    HTML stuff

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -18,6 +18,31 @@ def validate_html(html):
     # but arbitrary text located between the html tags
 
 
+    if type(html) == list:
+        html = ''.join(html)
+
+    # Base case
+    if len(html) == 0:
+        return True
+
+    # Run extract tags
+    taglist = _extract_tags(html)
+    stack = []
+
+    if taglist[0][0] == '<':
+        stack.append(taglist[0])
+        f = list(taglist[0])
+        f.insert(1, '/')
+        if f == list(taglist[-1]):
+            stack.pop()
+            return validate_html(taglist[1:-1])
+        elif f == list(taglist[1]):
+            stack.pop()
+            return validate_html(taglist[2:])
+        else:
+            return False
+
+
 def _extract_tags(html):
     '''
     This is a helper function for `validate_html`.
@@ -29,3 +54,22 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+
+
+    l = []
+    l2 = []
+    if len(html) == 0:
+        return l2
+    elif '<' not in html:
+        return l2
+    else:
+        for i in range(len(html)):
+            if html[i] == '<':
+                for j in range(i, len(html)):
+                    if html[j] == '>':
+                        l.append(html[i:j + 1])
+        for i in range(len(l)):
+            if l[i].count('<') == 1:
+                l2.append(l[i])
+
+        return l2
