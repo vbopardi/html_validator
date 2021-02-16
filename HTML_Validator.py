52ff185c5cbbe4a86b8985da657f8a1db5182a3b
vbopardi <vbopard@gmail.com>
2021-02-16 00:37:00 -0800

    Flake8

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -3,20 +3,9 @@
 
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
-
-    >>> validate_html('<strong>example</strong>')
-    True
-    >>> validate_html('<strong>example')
-    False
+    Checks whether every opening tag has a
+    corresponding closing tag.
     '''
-
-    # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the class/book
-    # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
-    # but arbitrary text located between the html tags
-
 
     if type(html) == list:
         html = ''.join(html)
@@ -45,18 +34,9 @@ def validate_html(html):
 
 def _extract_tags(html):
     '''
-    This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
-
-    This function returns a list of all the html tags contained in the input string,
-    stripping out all text not contained within angle brackets.
-
-    >>> _extract_tags('Python <strong>rocks</strong>!')
-    ['<strong>', '</strong>']
+    Extract HTML tags from string
     '''
-
-
-    l = []
+    l1 = []
     l2 = []
     if len(html) == 0:
         return l2
@@ -67,9 +47,9 @@ def _extract_tags(html):
             if html[i] == '<':
                 for j in range(i, len(html)):
                     if html[j] == '>':
-                        l.append(html[i:j + 1])
-        for i in range(len(l)):
-            if l[i].count('<') == 1:
-                l2.append(l[i])
+                        l1.append(html[i:j + 1])
+        for i in range(len(l1)):
+            if l1[i].count('<') == 1:
+                l2.append(l1[i])
 
         return l2
